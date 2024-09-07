e09297e94e0b7a265cf7136224e4168e5beb13d7
KamrulEng7876 <kamrulhasansabug786@gmail.com>
2024-09-07 23:55:25 +0600

    initial Commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,189 +1,30 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+var myBagAppBar = AppBar(
+  backgroundColor: Colors.grey.shade300,
+  title: const Text(
+    'My Bag',
+    style: TextStyle(
+      fontSize: 32,
+      fontWeight: FontWeight.w500,
+    ),
+  ),
+);
 
-  @override
-  State<HomeScreen> createState() => _HomeActivityState();
-}
+class BagItem {
+  final String productImage;
+  final String productTitle;
+  final String color;
+  final String size;
+  final double price;
+  int quantity;
 
-class _HomeActivityState extends State<HomeScreen> {
-  final TextEditingController _glassNoTEController = TextEditingController(
-    text: '1',
-  );
-  List<WaterTrack> waterTrackLists = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Inventory App'),
-        backgroundColor: Colors.greenAccent,
-        titleSpacing: 10,
-        //text space
-        // centerTitle: true, //title center
-        toolbarHeight: 60,
-        //Appbar height
-        toolbarOpacity: 1,
-        //color garo kora text hide kora
-        titleTextStyle: TextStyle(
-            color: Colors.red,
-            fontSize: 25.0,
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.bold),
-        actions: [
-          IconButton(
-              onPressed: () {
-                final snackbar = SnackBar(content: Text('I ame comment!'));
-                ScaffoldMessenger.of(context).showSnackBar(snackbar);
-                ;
-              },
-              icon: Icon(Icons.comment)),
-          IconButton(
-              onPressed: () {
-                final snackbar = SnackBar(content: Text('I ame search!'));
-                ScaffoldMessenger.of(context).showSnackBar(snackbar);
-                ;
-              },
-              icon: Icon(Icons.search)),
-          IconButton(
-              onPressed: () {
-                final snackbar = SnackBar(content: Text('I ame setting!'));
-                ScaffoldMessenger.of(context).showSnackBar(snackbar);
-                ;
-              },
-              icon: Icon(Icons.settings)),
-          IconButton(
-              onPressed: () {
-                final snackbar = SnackBar(content: Text('I ame email!'));
-                ScaffoldMessenger.of(context).showSnackBar(snackbar);
-                ;
-              },
-              icon: Icon(
-                Icons.email,
-                color: Colors.white,
-              )),
-          IconButton(
-              onPressed: () {
-                final snackbar = SnackBar(content: Text('I ame favorite!'));
-                ScaffoldMessenger.of(context).showSnackBar(snackbar);
-                ;
-              },
-              icon: Icon(
-                Icons.favorite,
-                color: Colors.red,
-              ))
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          _buildWaterTrackCounter(),
-          SizedBox(
-            height: 24,
-          ),
-          _buidWaterTrackList()
-        ],
-      ),
-    );
-  }
-
-  Widget _buidWaterTrackList() {
-    return Expanded(
-            child: ListView.separated(
-        itemCount: waterTrackLists.length,
-                itemBuilder: (context, index) {
-          final WaterTrack waterTrack=waterTrackLists[index];
-                  return ListTile(
-                    tileColor: Colors.white,
-                    title: Text('Time'),
-                    subtitle: Text('Date'),
-                    leading: CircleAvatar(child: Text('${waterTrack.noOfGlasses}')),
-                    trailing: IconButton(
-                      onPressed:()=> _onTapDeletButton(index),
-                      icon: Icon(Icons.delete_forever),
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return Divider();
-                   },
-            )
-    );
-  }
-
-
-  Widget _buildWaterTrackCounter() {
-    return Column(
-          children: [
-            Text(
-              getTotalGlassCount().toString(),
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            Text(
-              'Glass/S',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                    width: 60,
-                    child: TextField(
-                      controller: _glassNoTEController,
-                      keyboardType: TextInputType.number,
-                      textAlign: TextAlign.center,
-                    )),
-                SizedBox(
-                  width: 24,
-                ),
-                ElevatedButton(onPressed: _onTapAddWaterTrack, child: Text('Add')),
-              ],
-            ),
-          ],
-        );
-  }
-
-
-  int getTotalGlassCount(){
-    int counter =0;
-    for (WaterTrack t in waterTrackLists){
-      counter+=t.noOfGlasses;
-    }
-    return counter;
-  }
-
-  void _onTapAddWaterTrack() {
-    if (_glassNoTEController.text.isEmpty) {
-      _glassNoTEController.text = '1';
-    }
-    final int noOfGlasses = int.tryParse(_glassNoTEController.text) ?? 1;
-    WaterTrack waterTrack = WaterTrack(
-      noOfGlasses: noOfGlasses,
-      dateTime: DateTime.now(),
-    );
-    waterTrackLists.add(waterTrack);
-    setState(() {
-
-    });
-  }
-  void _onTapDeletButton(int index){
-    waterTrackLists.removeAt(index);
-    setState(() {
-
-    });
-  }
-}
-
-class WaterTrack {
-  final int noOfGlasses;
-  final DateTime dateTime;
-
-  WaterTrack({required this.noOfGlasses, required this.dateTime});
+  BagItem({
+    required this.productImage,
+    required this.productTitle,
+    required this.color,
+    required this.size,
+    required this.price,
+    this.quantity = 1,
+  });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,229 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_batch_seven/home_screen.dart';
-
-
-
-
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: MyBagScreen(),
     );
   }
 }
 
+class MyBagScreen extends StatefulWidget {
+  @override
+  _MyBagScreenState createState() => _MyBagScreenState();
+}
+
+class _MyBagScreenState extends State<MyBagScreen> {
+  final List<CartItem> _cartItems = [
+    CartItem(imageUrl: 'https://via.placeholder.com/100', productName: 'Pullover', color: 'Black', size: 'L', price: 51, quantity: 1),
+    CartItem(imageUrl: 'https://via.placeholder.com/100', productName: 'T-Shirt', color: 'Gray', size: 'L', price: 30, quantity: 1),
+    CartItem(imageUrl: 'https://via.placeholder.com/100', productName: 'Sport Dress', color: 'Black', size: 'M', price: 43, quantity: 1),
+  ];
+
+  int get _totalAmount {
+    return _cartItems.fold(0, (sum, item) => sum + item.price * item.quantity);
+  }
+
+  void _updateTotal() {
+    setState(() {});
+  }
+
+  void _showCheckoutMessage() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Congratulations! Your order has been placed.'),
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Bag"),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: _cartItems.length,
+                itemBuilder: (context, index) {
+                  return CartItemWidget(
+                    cartItem: _cartItems[index],
+                    onQuantityChanged: _updateTotal,
+                  );
+                },
+              ),
+            ),
+            TotalAmount(total: _totalAmount),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _showCheckoutMessage,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                padding: EdgeInsets.symmetric(vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  'CHECK OUT',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CartItem {
+  final String imageUrl;
+  final String productName;
+  final String color;
+  final String size;
+  final int price;
+  int quantity;
+
+  CartItem({
+    required this.imageUrl,
+    required this.productName,
+    required this.color,
+    required this.size,
+    required this.price,
+    this.quantity = 1,
+  });
+}
+
+class CartItemWidget extends StatelessWidget {
+  final CartItem cartItem;
+  final VoidCallback onQuantityChanged;
+
+  CartItemWidget({
+    required this.cartItem,
+    required this.onQuantityChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 16),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            blurRadius: 10,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Image.network(cartItem.imageUrl, width: 70, height: 70),
+          SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  cartItem.productName,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Text('Color: ${cartItem.color}, Size: ${cartItem.size}'),
+                Text('\$${cartItem.price}', style: TextStyle(fontSize: 18)),
+              ],
+            ),
+          ),
+          QuantitySelector(
+            cartItem: cartItem,
+            onQuantityChanged: onQuantityChanged,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class QuantitySelector extends StatefulWidget {
+  final CartItem cartItem;
+  final VoidCallback onQuantityChanged;
+
+  QuantitySelector({
+    required this.cartItem,
+    required this.onQuantityChanged,
+  });
+
+  @override
+  _QuantitySelectorState createState() => _QuantitySelectorState();
+}
+
+class _QuantitySelectorState extends State<QuantitySelector> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          icon: Icon(Icons.remove),
+          onPressed: () {
+            setState(() {
+              if (widget.cartItem.quantity > 1) {
+                widget.cartItem.quantity--;
+                widget.onQuantityChanged();
+              }
+            });
+          },
+        ),
+        Text('${widget.cartItem.quantity}', style: TextStyle(fontSize: 18)),
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              widget.cartItem.quantity++;
+              widget.onQuantityChanged();
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class TotalAmount extends StatelessWidget {
+  final int total;
+
+  TotalAmount({required this.total});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Total amount:',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          '\$$total',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
